5e45ee838a3f01b3fccfa8da7f506d0428ffdbf6
김동섭 <dongsub@gimdongseobs-MacBook-Pro.local>
2024-10-29 03:07:44 -0400

    v2_2

--- application.py
+++ application.py
@@ -17,7 +17,7 @@ from server import application
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', action='store_true')
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
     if args.l:
         application.debug = True
         application.run(host='0.0.0.0',port=8000)    
