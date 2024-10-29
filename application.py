c1363bfc83064ec059941e437a5efdd5653c808c
김동섭 <dongsub@gimdongseobs-MacBook-Pro.local>
2024-10-29 17:51:46 -0400

    removed global variable args l

--- application.py
+++ application.py
@@ -1,15 +1,3 @@
-# from server import application
-# from server import args
-
-# # run the application.
-# if __name__ == "__main__":
-#     if args.l:
-#         print("Local server is running ...")
-#         application.debug = True
-#         application.run(host='0.0.0.0',port=8000)    
-#     else:
-#         application.run()
-
 # import argparse
 from server import application
 
--- server/__init__.py
+++ server/__init__.py
@@ -3,12 +3,6 @@
 import flask
 from flask_cors import CORS
 from flask_mysqldb import MySQL
-import argparse
-
-# global variable for commandline argument tracking
-parser = argparse.ArgumentParser()
-parser.add_argument('-l', action='store_true')
-args = parser.parse_args()
 
 # index page texts
 header_text = '''
