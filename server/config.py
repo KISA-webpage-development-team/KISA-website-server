d57a43c48154372c675771b23d8e5e7f13aa58a0
김동섭 <dongsubk@umich.edu>
2025-02-13 17:53:12 -0500

    config.py is safe to upload

---
+++ server/config.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file (only in local dev)
+load_dotenv()
+
+"""KISA website development configuration."""
+
+# Root of this application
+APPLICATION_ROOT = '/'
+
+# Secret key for encrypting cookies
+SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")  # Default for safety
+
+# Session cookie name
+SESSION_COOKIE_NAME = 'login'
+
+# File Upload to CloudFront
+CLOUDFRONT_URL = os.getenv("CLOUDFRONT_URL", "https://d1jb1ppquwym6d.cloudfront.net")
+
+# MySQL Configurations
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+
+# Database selection
+MYSQL_DB = 'testdb' if os.getenv('FLASK_ENV') == 'development' else 'ebdb'
