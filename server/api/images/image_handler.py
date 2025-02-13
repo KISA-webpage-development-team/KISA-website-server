50df8c1e17c50737046cf2718af922875aed495b
김동섭 <dongsubk@umich.edu>
2025-02-13 17:51:56 -0500

    image handler complete

---
+++ server/api/images/image_handler.py
@@ -0,0 +1,90 @@
+import server
+import os
+from ..helpers import extract_temp_keys, extract_uploaded_keys, replace_temp_srcs
+
+def upload_imgs(text, postid):
+    """
+    Uploads images in a text
+    """
+    # Base url for images for display on editor is a s3 url
+    temp_keys = extract_temp_keys(text)
+        
+    # Construct list of new scrs
+    new_urls = []
+    for i, temp_key in enumerate(temp_keys):
+        extension = temp_key.split(".")[-1]
+        directory = "test_images" if os.getenv("FLASK_ENV") == 'development' else "images"
+        new_urls.append(
+            f"{os.getenv('CLOUDFRONT_URL')}/{directory}/post_{postid}_{i}.{extension}"
+        )
+
+    # Initiate AWS clients
+    client = server.model.AWSClient()
+
+    # Locate s3 images with the keys and move them to '/images', with new keys
+    for temp_key, new_url in zip(temp_keys, new_urls):
+        # Construct new key
+        new_key = new_url.split(f"{os.getenv('CLOUDFRONT_URL')}/")[1]
+
+        client.move_object(temp_key, new_key)
+    
+    # return modified text to upper scope
+    return replace_temp_srcs(text, new_urls)
+
+
+def update_imgs(new_text, prev_text, postid):
+    """
+    Handle image upload and deletion according to the new and previous text
+    """
+    # Extract key information from the previous and new text
+    # existing_keys = set(extract_keys(prev_text))
+    existing_keys = set(extract_uploaded_keys(prev_text))
+    remaining_keys = set(extract_uploaded_keys(new_text))
+    keys_to_delete = list(existing_keys - remaining_keys)
+    keys_to_move = extract_temp_keys(new_text)
+
+    # Delete the images that does not remain in the new text anymore
+    client = server.model.AWSClient()
+    if keys_to_delete:
+        client.delete_uploaded_objects(keys_to_delete)
+
+    # Find maximum index of the images in the previous text
+    max_index = max(int(key.split('/')[1].split('_')[2].split('.')[0]) for key in existing_keys)
+
+    # Index for new images starts from +1 the max index
+    new_img_index = max_index + 1
+
+    # Move the temp images in the new text to the permanent folder
+    # Construct list of new scrs
+    new_urls = []
+    for i, key in enumerate(keys_to_move, start=new_img_index):
+        extension = key.split(".")[-1]
+        directory = "test_images" if os.getenv("FLASK_ENV") == 'development' else "images"
+        new_urls.append(
+            f"{os.getenv('CLOUDFRONT_URL')}/{directory}/post_{postid}_{i}.{extension}"
+        )
+
+    # Locate s3 images with the keys and move them to '/images', with new keys
+    for temp_key, new_url in zip(keys_to_move, new_urls):
+        # Construct new key
+        new_key = new_url.split(f"{os.getenv('CLOUDFRONT_URL')}/")[1]
+
+        # Move the image
+        client.move_object(temp_key, new_key)
+
+    # return modified text to upper scope
+    return replace_temp_srcs(new_text, new_urls)
+
+def delete_imgs(text):
+    """
+    Delete image files that were uploaded to the s3 bucket
+    """
+    # Initiate AWS clients
+    client = server.model.AWSClient()
+
+    # Extract the keys of the images that were uploaded before
+    keys_to_delete = extract_uploaded_keys(text)
+
+    # Delete the images
+    if keys_to_delete:
+        client.delete_uploaded_objects(keys_to_delete)
--- server/model.py
+++ server/model.py
@@ -1,6 +1,9 @@
 """KISAWEB model (connection with mySQL database)."""
 import server
 import MySQLdb.cursors
+import boto3
+import os
+import datetime
 
 class Cursor:
     def __init__(self):
@@ -27,3 +30,59 @@ class Cursor:
     def __del__(self):
         server.db.connection.commit()
         self.cursor.close()
+
+class AWSClient:
+    def __init__(self):
+        self.s3 = boto3.client('s3')
+        self.cloudfront = boto3.client('cloudfront')
+
+    def generate_presigned_url(self, intention, file_key, file_type):
+        params = {
+            "Bucket": os.getenv("S3_BUCKET_NAME"),
+            "Key": file_key,
+        }
+        if intention == "put_object":
+            params["ContentType"] = file_type
+
+        return self.s3.generate_presigned_url(
+            intention,
+            params,
+            ExpiresIn=3600
+        )
+    
+    def create_invalidation(self, invalidate_paths):
+        # cloudfront invalidation requires absolute path
+        invalidate_paths = [f"/{path}" for path in invalidate_paths]
+
+        self.cloudfront.create_invalidation(
+            DistributionId=os.getenv("CLOUDFRONT_DISTRIBUTION_ID"),
+            InvalidationBatch={
+                'Paths': {
+                    'Quantity': len(invalidate_paths),
+                    'Items': invalidate_paths
+                },
+                'CallerReference': str(datetime.datetime.now())
+            }
+        )
+
+    def delete_object(self, key):
+        self.s3.delete_object(
+            Bucket=os.getenv('S3_BUCKET_NAME'),
+            Key=key
+        )
+
+    def copy_object(self, key, new_key):
+        self.s3.copy_object(
+            Bucket=os.getenv("S3_BUCKET_NAME"),
+            CopySource={"Bucket": os.getenv("S3_BUCKET_NAME"), "Key": key},
+            Key=new_key
+        )
+
+    def move_object(self, key, new_key):
+        self.copy_object(key, new_key)
+        self.delete_object(key)
+
+    def delete_uploaded_objects(self, keys):
+        self.create_invalidation(keys)
+        for key in keys:
+            self.delete_object(key)
